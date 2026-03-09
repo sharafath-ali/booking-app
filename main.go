f9ab2922651598af16f0fb332888814c7ecdd9ac
sharafath ali <sharafathaliofficial@gmail.com>
2026-03-09 17:46:20 +0530

    feat: add user input for ticket booking details and confirmation message

--- main.go
+++ main.go
@@ -12,4 +12,19 @@ func main() {
 	fmt.Println("welcome to our ", conference, "booking system\n")
 	fmt.Printf("we have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Printf("get your tickets here to attend\n")
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+	remainingTickets = remainingTickets - userTickets
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conference)
 }
