613990e14eeb1203aef6feb7d9583c96ff7bcd87
sharafath ali <sharafathaliofficial@gmail.com>
2026-03-09 17:02:29 +0530

    update ticket information and improve booking prompts in main application

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import "fmt"
 
 func main() {
 	var conference string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
 	fmt.Printf("welcome to our " + conference + " booking system\n")
 	fmt.Printf(conference)
 	fmt.Printf("\n%s\n", conference)
 	fmt.Printf("welcome to our %s booking system\n", conference)
-	fmt.Printf("please book your tickets for %v\n", conference)
+	fmt.Printf("we have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Printf("get your tickets here to attend\n")
 }
