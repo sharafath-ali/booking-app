b07bf4b652cd47b8ff25cb0cdc5a5b255ec731cf
Sharafath Ali <sharafathaliofficial@gmail.com>
2026-03-13 23:09:41 +0530

    feat: Introduce a new `helper` package with `PrintWelcome` and `ExportedConstant`, and import it into the main package.

--- helper.go
+++ helper.go
@@ -1,8 +1,13 @@
 package main
 
-import "unicode"
+import (
+	"bookingapp/helper"
+	"unicode"
+)
 
 func isValidName(s string) bool {
+	// this is how we access the exported constant from the helper package
+	const ExportedConstant = helper.ExportedConstant
 	for _, r := range s {
 		if !unicode.IsLetter(r) {
 			return false
--- helper/helper.go
+++ helper/helper.go
@@ -2,6 +2,8 @@ package helper
 
 import "fmt"
 
+const ExportedConstant = "This is an exported constant"
+
 func PrintWelcome(conference string, conferenceTickets int, remainingTickets uint) {
 	fmt.Printf("welcome to our " + conference + " booking system\n")
 	fmt.Printf(conference)
